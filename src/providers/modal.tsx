31f6357dab19f08a31fda615327c847c320df019
IgorMontezuma20 <igorminranda17@gmail.com>
2024-01-08 23:20:03 -0300

    feat: Ajustando detalhes do modal

--- src/providers/modal.tsx
+++ src/providers/modal.tsx
@@ -10,19 +10,26 @@ import { TicketModal } from "@/components/modal";
 interface ModalContextData {
   visible: boolean;
   handleModalVisible: () => void;
+  ticket: TicketInfo | undefined;
+}
+
+interface TicketInfo {
+  ticket: TicketProps;
+  customer: CustomerProps | null;
 }
 
 export const ModalContext = createContext({} as ModalContextData);
 
 export const ModalProvider = ({ children }: { children: ReactNode }) => {
-  const [visible, setVisible] = useState(true);
+  const [visible, setVisible] = useState(false);
+  const [ticket, setTicket] = useState<TicketInfo>();
 
   function handleModalVisible() {
     setVisible(!visible);
   }
 
   return (
-    <ModalContext.Provider value={{ visible, handleModalVisible }}>
+    <ModalContext.Provider value={{ visible, handleModalVisible, ticket }}>
       {visible && <TicketModal />}
       {children}
     </ModalContext.Provider>
