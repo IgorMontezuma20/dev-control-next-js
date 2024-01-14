931305d6a0d7d904db9de75c0b372e4ad1377694
IgorMontezuma20 <igorminranda17@gmail.com>
2024-01-14 14:43:26 -0300

    feat: Correcão do provider do Google

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -11,7 +11,7 @@ export const authOptions: AuthOptions = {
       clientSecret: "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"
     })
   ],
-  secret: process.env.SECRET,
+  secret: "projeto123",
   callbacks: {
     async session({ session, token, user, }){
       session.user = { ...session.user, id: user.id } as {
